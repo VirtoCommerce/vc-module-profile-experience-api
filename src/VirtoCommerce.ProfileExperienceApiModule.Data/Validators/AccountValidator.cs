105fe590e527ff518653f22b003e1f5ecc143675
Getmansky <mge@virtoway.com>
2022-05-26 16:48:07 +0300

    Email validation is added

--- src/VirtoCommerce.ProfileExperienceApiModule.Data/Validators/AccountValidator.cs
+++ src/VirtoCommerce.ProfileExperienceApiModule.Data/Validators/AccountValidator.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.ProfileExperienceApiModule.Data.Models.RegisterCompany;
 
 namespace VirtoCommerce.ProfileExperienceApiModule.Data.Validators
@@ -9,7 +10,11 @@ namespace VirtoCommerce.ProfileExperienceApiModule.Data.Validators
         {
             RuleFor(x => x.UserName).NotNull().NotEmpty();
             RuleFor(x => x.Password).NotNull().NotEmpty();
-            RuleFor(x => x.Email).NotNull().NotEmpty();
+            RuleFor(x => x.Email)
+                .NotNull()
+                .NotEmpty()
+                .Must(x => x.IsValidEmail())
+                .WithMessage("Invalid email format");
         }
     }
 }
