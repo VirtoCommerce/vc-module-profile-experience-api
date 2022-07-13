b9932b3d4420d5ff4ced07d485e764b5ecdf079e
Leysan Nigmatullina <leysunny21@gmail.com>
2022-07-13 18:27:48 +0300

    PT-8249: Fix dynamic properties for register member types

--- src/VirtoCommerce.ProfileExperienceApiModule.Data/Schemas/RegisterCompany/RegisterOrganizationType.cs
+++ src/VirtoCommerce.ProfileExperienceApiModule.Data/Schemas/RegisterCompany/RegisterOrganizationType.cs
@@ -1,22 +1,29 @@
 using System.Linq;
 using GraphQL.Types;
 using VirtoCommerce.CustomerModule.Core.Model;
+using VirtoCommerce.ExperienceApiModule.Core.Extensions;
+using VirtoCommerce.ExperienceApiModule.Core.Helpers;
 using VirtoCommerce.ExperienceApiModule.Core.Schemas;
+using VirtoCommerce.ExperienceApiModule.Core.Services;
 
 namespace VirtoCommerce.ProfileExperienceApiModule.Data.Schemas.RegisterCompany
 {
-    public class RegisterOrganizationType : ObjectGraphType
+    public class RegisterOrganizationType : ExtendableGraphType<Organization>
     {
-        public RegisterOrganizationType()
+        public RegisterOrganizationType(IDynamicPropertyResolverService dynamicPropertyResolverService)
         {
             Field<NonNullGraphType<StringGraphType>>("id");
             Field<NonNullGraphType<StringGraphType>>("name");
             Field<StringGraphType>("description");
-            Field<ListGraphType<DynamicPropertyValueType>>(nameof(Member.DynamicProperties));
-            Field<MemberAddressType>("address", resolve: context => (context.Source as Organization).Addresses?.FirstOrDefault());
+            Field<MemberAddressType>("address", resolve: context => (context.Source).Addresses?.FirstOrDefault());
             Field<StringGraphType>("status");
             Field<StringGraphType>("createdBy");
             Field<StringGraphType>("ownerId");
+            ExtendableField<NonNullGraphType<ListGraphType<DynamicPropertyValueType>>>(
+                "dynamicProperties",
+                "Contact's dynamic property values",
+                QueryArgumentPresets.GetArgumentForDynamicProperties(),
+                context => dynamicPropertyResolverService.LoadDynamicPropertyValues(context.Source, context.GetArgumentOrValue<string>("cultureName")));
         }
     }
 }
