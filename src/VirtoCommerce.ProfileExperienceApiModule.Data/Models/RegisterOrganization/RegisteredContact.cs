3b128e31f520b3767efed267579f41920a4079b2
Leysan Nigmatullina <86958548+einlied753@users.noreply.github.com>
2022-07-15 11:46:48 +0300

    PT-8151: Add About field for Contact (#17)

--- src/VirtoCommerce.ProfileExperienceApiModule.Data/Models/RegisterOrganization/RegisteredContact.cs
+++ src/VirtoCommerce.ProfileExperienceApiModule.Data/Models/RegisterOrganization/RegisteredContact.cs
@@ -11,6 +11,7 @@ namespace VirtoCommerce.ProfileExperienceApiModule.Data.Models.RegisterOrganizat
         public string MiddleName { get; set; }
         public DateTime Birthdate { get; set; }
         public string PhoneNumber { get; set; }
+        public string About { get; set; }
         public IList<DynamicPropertyValue> DynamicProperties { get; set; }
     }
 }
