afbe9706c85600f05d1758ff3e0393d35dcb7eef
Getmansky <mge@virtoway.com>
2022-08-26 12:54:34 +0300

    Possibility of roles assignment to the invited user

--- src/VirtoCommerce.ProfileExperienceApiModule.Data/Schemas/InputInviteUserType.cs
+++ src/VirtoCommerce.ProfileExperienceApiModule.Data/Schemas/InputInviteUserType.cs
@@ -12,6 +12,7 @@ namespace VirtoCommerce.ProfileExperienceApiModule.Data.Schemas
             Field<StringGraphType>(nameof(InviteUserCommand.UrlSuffix), "Optional URL suffix: you may provide here relative URL to your page which handle registration by invite");
             Field<NonNullGraphType<ListGraphType<NonNullGraphType<StringGraphType>>>>(nameof(InviteUserCommand.Emails), "Emails which will receive invites");
             Field<StringGraphType>(nameof(InviteUserCommand.Message), "Optional message to include into email with instructions which invites persons will see");
+            Field<ListGraphType<NonNullGraphType<StringGraphType>>>(nameof(InviteUserCommand.RoleIds), "Role IDs or names to be assigned to the invited user");
         }
     }
 }
