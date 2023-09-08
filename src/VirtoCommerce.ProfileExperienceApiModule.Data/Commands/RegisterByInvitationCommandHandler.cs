0a00c93d1b4840db79190a2447710ba27f7bb4e5
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2023-09-08 09:31:32 +0200

    PT-13378: verify email on register by invitation (#60)

--- src/VirtoCommerce.ProfileExperienceApiModule.Data/Commands/RegisterByInvitationCommandHandler.cs
+++ src/VirtoCommerce.ProfileExperienceApiModule.Data/Commands/RegisterByInvitationCommandHandler.cs
@@ -9,82 +9,115 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Hosting;
 using VirtoCommerce.CustomerModule.Core.Model;
 using VirtoCommerce.CustomerModule.Core.Services;
+using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Security;
 using VirtoCommerce.ProfileExperienceApiModule.Data.Extensions;
-using VirtoCommerce.ProfileExperienceApiModule.Data.Models;
 using VirtoCommerce.ProfileExperienceApiModule.Data.Queries;
+using VirtoCommerce.StoreModule.Core.Services;
 
 namespace VirtoCommerce.ProfileExperienceApiModule.Data.Commands
 {
     public class RegisterByInvitationCommandHandler : IRequestHandler<RegisterByInvitationCommand, IdentityResultResponse>
     {
         private readonly IWebHostEnvironment _environment;
-        private readonly Func<UserManager<ApplicationUser>> _userManagerFactory;
         private readonly IMemberService _memberService;
+        private readonly IStoreService _storeService;
+        private readonly IMediator _mediator;
+        private readonly Func<UserManager<ApplicationUser>> _userManagerFactory;
 
         public RegisterByInvitationCommandHandler(
             IWebHostEnvironment environment,
-            Func<UserManager<ApplicationUser>> userManager, IMemberService memberService)
+            Func<UserManager<ApplicationUser>> userManager,
+            IMemberService memberService,
+            IStoreService storeService,
+            IMediator mediator)
         {
             _environment = environment;
             _userManagerFactory = userManager;
             _memberService = memberService;
+            _storeService = storeService;
+            _mediator = mediator;
         }
 
         public virtual async Task<IdentityResultResponse> Handle(RegisterByInvitationCommand request, CancellationToken cancellationToken)
         {
             using var userManager = _userManagerFactory();
 
-            var result = new IdentityResultResponse
-            {
-                Errors = new List<IdentityErrorInfo>(),
-                Succeeded = true,
-            };
-
-            IdentityResult identityResult;
-
             var user = await userManager.FindByIdAsync(request.UserId);
 
-            // PT-6083: reduce complexity
             if (user == null)
             {
                 var errors = _environment.IsDevelopment() ? new[] { new IdentityError { Code = "UserNotFound", Description = "User not found" } } : null;
+                return SetResponse(IdentityResult.Failed(errors));
+            }
+
+            var identityResult = await userManager.ResetPasswordAsync(user, Uri.UnescapeDataString(request.Token), request.Password);
+            if (!identityResult.Succeeded)
+            {
+                return SetResponse(identityResult);
+            }
+
+            identityResult = await userManager.SetUserNameAsync(user, request.Username);
+            if (!identityResult.Succeeded)
+            {
+                return SetResponse(identityResult);
+            }
+
+            user.EmailConfirmed = true;
+            identityResult = await userManager.UpdateAsync(user);
+            if (!identityResult.Succeeded)
+            {
+                return SetResponse(identityResult);
+            }
+
+            var contact = await _memberService.GetByIdAsync(user.MemberId) as Contact;
+            if (contact == null)
+            {
+                var errors = _environment.IsDevelopment() ? new[] { new IdentityError { Code = "ContactNotFound", Description = "Contact not found" } } : null;
                 identityResult = IdentityResult.Failed(errors);
             }
             else
             {
-                identityResult = await userManager.ResetPasswordAsync(user, Uri.UnescapeDataString(request.Token), request.Password);
-                if (identityResult.Succeeded)
+                contact.FirstName = request.FirstName;
+                contact.LastName = request.LastName;
+                contact.FullName = $"{request.FirstName} {request.LastName}";
+                if (!string.IsNullOrEmpty(request.Phone))
                 {
-                    identityResult = await userManager.SetUserNameAsync(user, request.Username);
-                    if (identityResult.Succeeded)
-                    {
-                        var contact = await _memberService.GetByIdAsync(user.MemberId) as Contact;
-                        if (contact == null)
-                        {
-                            var errors = _environment.IsDevelopment() ? new[] { new IdentityError { Code = "ContactNotFound", Description = "Contact not found" } } : null;
-                            identityResult = IdentityResult.Failed(errors);
-                        }
-                        else
-                        {
-                            contact.FirstName = request.FirstName;
-                            contact.LastName = request.LastName;
-                            contact.FullName = $"{request.FirstName} {request.LastName}";
-                            if (!string.IsNullOrEmpty(request.Phone))
-                            {
-                                contact.Phones = new List<string> { request.Phone };
-                            }
-
-                            await _memberService.SaveChangesAsync(new Member[] { contact });
-                        }
-                    }
+                    contact.Phones = new List<string> { request.Phone };
                 }
+
+                await _memberService.SaveChangesAsync(new Member[] { contact });
+
+                await SendRegistrationNotification(user, contact, cancellationToken);
             }
 
-            result.Errors = identityResult.Errors.Select(x => x.MapToIdentityErrorInfo()).ToList();
-            result.Succeeded = identityResult.Succeeded;
-
-            return result;
+            return SetResponse(identityResult);
         }
+
+        private static IdentityResultResponse SetResponse(IdentityResult identityResult) => new()
+        {
+            Errors = identityResult.Errors.Select(x => x.MapToIdentityErrorInfo()).ToList(),
+            Succeeded = identityResult.Succeeded,
+        };
+
+        private async Task SendRegistrationNotification(ApplicationUser user, Contact contact, CancellationToken cancellationToken)
+        {
+            var store = await _storeService.GetByIdAsync(user.StoreId);
+            if (store == null)
+            {
+                return;
+            }
+
+            var registrationNotificationRequest = new SendRegistrationNotificationCommand
+            {
+                Store = store,
+                LanguageCode = contact.DefaultLanguage ?? store.DefaultLanguage,
+                Contact = contact,
+            };
+
+            var cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+            await _mediator.Send(registrationNotificationRequest, cancellationTokenSource.Token);
+        }
+
     }
 }
