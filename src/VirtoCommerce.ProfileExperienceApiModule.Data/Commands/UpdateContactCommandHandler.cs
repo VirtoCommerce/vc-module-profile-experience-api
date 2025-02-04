b3c07fc243731091ce9e21eee9ff7b62d5e2d5e6
Denis Vasichkin <denis.vasichkin@gmail.com>
2025-02-04 17:18:30 +0300

    refactor: UpdateContactCommandHandler Extract Mapping Logic into Separate Method

--- src/VirtoCommerce.ProfileExperienceApiModule.Data/Commands/UpdateContactCommandHandler.cs
+++ src/VirtoCommerce.ProfileExperienceApiModule.Data/Commands/UpdateContactCommandHandler.cs
@@ -2,8 +2,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using MediatR;
-using VirtoCommerce.Xapi.Core.Services;
+using VirtoCommerce.CustomerModule.Core.Model;
 using VirtoCommerce.ProfileExperienceApiModule.Data.Aggregates.Contact;
+using VirtoCommerce.Xapi.Core.Services;
 
 namespace VirtoCommerce.ProfileExperienceApiModule.Data.Commands
 {
@@ -26,7 +27,7 @@ namespace VirtoCommerce.ProfileExperienceApiModule.Data.Commands
         {
             var contactAggregate = await _contactAggregateRepository.GetMemberAggregateRootByIdAsync<ContactAggregate>(request.Id);
 
-            _mapper.Map(request, contactAggregate.Contact);
+            UpdateContact(request, contactAggregate.Contact);
 
             if (request.DynamicProperties != null)
             {
@@ -36,6 +37,11 @@ namespace VirtoCommerce.ProfileExperienceApiModule.Data.Commands
             await _contactAggregateRepository.SaveAsync(contactAggregate);
 
             return contactAggregate;
+        }
+
+        protected virtual void UpdateContact(UpdateContactCommand request, Contact contact)
+        {
+            _mapper.Map(request, contact);
         }
     }
 }
