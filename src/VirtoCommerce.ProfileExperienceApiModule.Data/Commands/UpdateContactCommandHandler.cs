d6844da09241599a8a13587813d06f609ee627f0
Artem Dudarev <artem@virtoworks.com>
2025-06-09 16:54:06 +0200

    VCST-3294: Save selected address for each organization (#113)

--- src/VirtoCommerce.ProfileExperienceApiModule.Data/Commands/UpdateContactCommandHandler.cs
+++ src/VirtoCommerce.ProfileExperienceApiModule.Data/Commands/UpdateContactCommandHandler.cs
@@ -2,45 +2,44 @@ using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using MediatR;
+using VirtoCommerce.CustomerModule.Core.Extensions;
 using VirtoCommerce.CustomerModule.Core.Model;
+using VirtoCommerce.CustomerModule.Core.Services;
 using VirtoCommerce.ProfileExperienceApiModule.Data.Aggregates.Contact;
 using VirtoCommerce.Xapi.Core.Services;
 
 namespace VirtoCommerce.ProfileExperienceApiModule.Data.Commands
 {
-    public class UpdateContactCommandHandler : IRequestHandler<UpdateContactCommand, ContactAggregate>
+    public class UpdateContactCommandHandler(
+        IContactAggregateRepository contactAggregateRepository,
+        IDynamicPropertyUpdaterService dynamicPropertyUpdater,
+        ICustomerPreferenceService customerPreferenceService,
+        IMapper mapper)
+        : IRequestHandler<UpdateContactCommand, ContactAggregate>
     {
-        private readonly IContactAggregateRepository _contactAggregateRepository;
-        private readonly IDynamicPropertyUpdaterService _dynamicPropertyUpdater;
-        private readonly IMapper _mapper;
-
-        public UpdateContactCommandHandler(IContactAggregateRepository contactAggregateRepository,
-            IDynamicPropertyUpdaterService dynamicPropertyUpdater,
-            IMapper mapper)
-        {
-            _contactAggregateRepository = contactAggregateRepository;
-            _dynamicPropertyUpdater = dynamicPropertyUpdater;
-            _mapper = mapper;
-        }
-
         public virtual async Task<ContactAggregate> Handle(UpdateContactCommand request, CancellationToken cancellationToken)
         {
-            var contactAggregate = await _contactAggregateRepository.GetMemberAggregateRootByIdAsync<ContactAggregate>(request.Id);
+            var contactAggregate = await contactAggregateRepository.GetMemberAggregateRootByIdAsync<ContactAggregate>(request.Id);
 
             await UpdateContactAsync(contactAggregate.Contact, request);
 
-            await _contactAggregateRepository.SaveAsync(contactAggregate);
+            await contactAggregateRepository.SaveAsync(contactAggregate);
 
             return contactAggregate;
         }
 
         protected virtual async Task UpdateContactAsync(Contact contact, UpdateContactCommand request)
         {
-            _mapper.Map(request, contact);
+            mapper.Map(request, contact);
 
             if (request.DynamicProperties != null)
             {
-                await _dynamicPropertyUpdater.UpdateDynamicPropertyValues(contact, request.DynamicProperties);
+                await dynamicPropertyUpdater.UpdateDynamicPropertyValues(contact, request.DynamicProperties);
+            }
+
+            if (request.SelectedAddressId != null)
+            {
+                await customerPreferenceService.SaveSelectedAddressId(request.UserId, request.OrganizationId, request.SelectedAddressId);
             }
         }
     }
