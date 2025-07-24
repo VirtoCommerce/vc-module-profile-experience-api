ac6b7de2690eb6e060598def8f2da91c6ff10088
Oleg Zhuk <zhukoo@gmail.com>
2025-07-24 19:22:36 +0200

    VCST-3663: User groups are not scoped within a current company.
    fix: User groups are not scoped within a current company.

--- src/VirtoCommerce.ProfileExperienceApiModule.Data/Middlewares/LoadUserToEvalContextMiddleware.cs
+++ src/VirtoCommerce.ProfileExperienceApiModule.Data/Middlewares/LoadUserToEvalContextMiddleware.cs
@@ -1,17 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using PipelineNet.Middleware;
-using VirtoCommerce.CoreModule.Core.Common;
 using VirtoCommerce.CustomerModule.Core.Model;
 using VirtoCommerce.CustomerModule.Core.Services;
 using VirtoCommerce.MarketingModule.Core.Model.Promotions;
-using VirtoCommerce.Platform.Core.Common;
-using VirtoCommerce.Platform.Core.DynamicProperties;
 using VirtoCommerce.PricingModule.Core.Model;
 using VirtoCommerce.TaxModule.Core.Model;
+using VirtoCommerce.Xapi.Core.Services;
 
 namespace VirtoCommerce.ProfileExperienceApiModule.Data.Middlewares
 {
@@ -19,20 +15,25 @@ namespace VirtoCommerce.ProfileExperienceApiModule.Data.Middlewares
     {
         private readonly IMapper _mapper;
         private readonly IMemberResolver _memberIdResolver;
-        private readonly IMemberService _memberService;
+        private readonly ILoadUserToEvalContextService _loadUserToEvalContextService;
 
-        public LoadUserToEvalContextMiddleware(IMapper mapper, IMemberResolver memberIdResolver, IMemberService memberService)
+        public LoadUserToEvalContextMiddleware(IMapper mapper, IMemberResolver memberIdResolver, ILoadUserToEvalContextService loadUserToEvalContextService)
         {
             _mapper = mapper;
             _memberIdResolver = memberIdResolver;
-            _memberService = memberService;
+            _loadUserToEvalContextService = loadUserToEvalContextService;
         }
 
         public async Task Run(PromotionEvaluationContext parameter, Func<PromotionEvaluationContext, Task> next)
         {
             if (!string.IsNullOrEmpty(parameter.CustomerId))
             {
-                await InnerSetShopperDataFromMember(parameter, parameter.CustomerId);
+                await _loadUserToEvalContextService.SetShopperDataFromMember(parameter, parameter.CustomerId);
+            }
+
+            if (!string.IsNullOrEmpty(parameter.OrganizaitonId))
+            {
+                await _loadUserToEvalContextService.SetShopperDataFromMember(parameter, parameter.OrganizaitonId);
             }
             await next(parameter);
         }
@@ -41,8 +42,14 @@ namespace VirtoCommerce.ProfileExperienceApiModule.Data.Middlewares
         {
             if (!string.IsNullOrEmpty(parameter.CustomerId))
             {
-                await InnerSetShopperDataFromMember(parameter, parameter.CustomerId);
+                await _loadUserToEvalContextService.SetShopperDataFromMember(parameter, parameter.CustomerId);
             }
+
+            if (!string.IsNullOrEmpty(parameter.OrganizationId))
+            {
+                await _loadUserToEvalContextService.SetShopperDataFromMember(parameter, parameter.OrganizationId);
+            }
+
             await next(parameter);
         }
 
@@ -59,39 +66,5 @@ namespace VirtoCommerce.ProfileExperienceApiModule.Data.Middlewares
             await next(parameter);
         }
 
-        private async Task InnerSetShopperDataFromMember(EvaluationContextBase evalContextBase, string customerId)
-        {
-            var member = await _memberIdResolver.ResolveMemberByIdAsync(customerId);
-            if (member is Contact contact)
-            {
-                evalContextBase.ShopperGender = contact.GetDynamicPropertyValue("gender", string.Empty);
-
-                if (contact.BirthDate != null)
-                {
-                    var zeroTime = new DateTime(1, 1, 1);
-                    var span = DateTime.UtcNow - contact.BirthDate.Value;
-                    evalContextBase.ShopperAge = (zeroTime + span).Year - 1;
-                }
-
-                evalContextBase.GeoTimeZone = contact.TimeZone;
-
-                evalContextBase.UserGroups = contact.Groups?.ToArray();
-
-                if (!contact.Organizations.IsNullOrEmpty())
-                {
-                    var userGroups = new List<string>();
-
-                    if (!evalContextBase.UserGroups.IsNullOrEmpty())
-                    {
-                        userGroups.AddRange(evalContextBase.UserGroups);
-                    }
-
-                    var organizations = await _memberService.GetByIdsAsync(contact.Organizations.ToArray(), MemberResponseGroup.WithGroups.ToString());
-                    userGroups.AddRange(organizations.OfType<Organization>().SelectMany(x => x.Groups));
-
-                    evalContextBase.UserGroups = userGroups.Distinct().ToArray();
-                }
-            }
-        }
     }
 }
